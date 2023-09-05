1900a7d8297f4449bce5ac40588e50e362667a82
jawdat-rana <jawdat_rana@hotmail.com>
2023-09-05 20:35:43 +0200

    Updated host and port for apps

--- application.py
+++ application.py
@@ -60,4 +60,4 @@ index_page = html.Div([
 app.layout = index_page
 
 if __name__ == '__main__':
-    application.run(debug=True, port=8080)
+    application.run(debug=True, host='0.0.0.0', port=10000)
--- dashboard_app.py
+++ dashboard_app.py
@@ -53,4 +53,4 @@ def update_graph(col_chosen):
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
